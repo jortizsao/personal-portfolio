6cd5a5ca1fadf00189484aa5de9d99f7c857a41b
Javier Ortiz <javier.ortizsaorin@gmail.com>
2018-08-13 00:11:25 +0200

    Add AboutMe

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <Home />
+  <div class="app">
+    <Home />
+    <Navigation />
+    <AboutMe />
+  </div>
 </template>
 <script>
 import Home from '@/components/Home.vue'
+import Navigation from '@/components/Navigation.vue'
+import AboutMe from '@/components/AboutMe.vue'
 
 export default {
   components: {
-    Home
+    Home,
+    Navigation,
+    AboutMe,
   }
 }
 </script>
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -219,6 +219,9 @@ jQuery(function ($) {
     }());
 
 
+
+
+
     // -------------------------------------------------------------
     // STELLAR FOR BACKGROUND SCROLLING
     // -------------------------------------------------------------
@@ -251,6 +254,38 @@ jQuery(function ($) {
 
     }());
 
+
+
+    // -------------------------------------------------------------
+    // Contact Form
+    // -------------------------------------------------------------
+
+    $('#contactForm').on('submit',function(e){
+
+        e.preventDefault();
+
+        var $action = $(this).prop('action');
+        var $data = $(this).serialize();
+        var $this = $(this);
+
+        $this.prevAll('.alert').remove();
+
+        $.post( $action, $data, function( data ) {
+
+            if( data.response=='error' ){
+
+                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
+            }
+
+            if( data.response=='success' ){
+
+                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
+                $this.find('input, textarea').val('');
+            }
+
+        }, "json");
+
+    });
 
 
 
