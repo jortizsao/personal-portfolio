6a6e85ad08f048f9e573606314e6c397835a5da0
Javier Ortiz <javier.ortizsaorin@gmail.com>
2018-08-13 12:44:45 +0200

    Add Skills section

---
+++ components/Skills.vue
@@ -0,0 +1,113 @@
+<template>
+  <section id="skills" class="skills-section section-padding">
+    <div class="container">
+      <h2 class="section-title wow fadeInUp">Skills</h2>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="skill-progress">
+            <div class="skill-title"><h3>Microservices</h3></div> 
+            <div class="progress">
+              <div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" ><span>91%</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="skill-progress">
+            <div class="skill-title"><h3>AWS</h3></div> 
+            <div class="progress">
+              <div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" ><span>85%</span>
+              </div>
+            </div>
+          </div>
+          <div class="skill-progress">
+            <div class="skill-title"><h3>GraphQL</h3></div>  
+            <div class="progress">
+              <div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" ><span>90%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="skill-progress">
+            <div class="skill-title"><h3>Node.js</h3></div> 
+            <div class="progress">
+              <div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="92" aria-valuemin="0" aria-valuemax="100" ><span>92%</span>
+              </div>
+            </div>
+          </div>
+          <div class="skill-progress">
+            <div class="skill-title"><h3>Vue.js</h3></div>  
+            <div class="progress">
+              <div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="91" aria-valuemin="0" aria-valuemax="100" ><span>91%</span>
+              </div>
+            </div>
+          </div>
+          <div class="skill-progress">
+            <div class="skill-title"><h3>Kubernetes</h3></div> 
+            <div class="progress">
+              <div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" ><span>80%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="skill-chart text-center">
+        <h3>More skills</h3>
+      </div>
+        
+      <div class="row more-skill text-center">
+        <div class="col-xs-12 col-sm-4 col-md-2">
+            <div class="chart" data-percent="90" data-color="e74c3c">
+                  <span class="percent"></span>
+                  <div class="chart-text">
+                    <span>Leadership</span>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-2">
+            <div class="chart" data-percent="88" data-color="3498db">
+                  <span class="percent"></span>
+                  <div class="chart-text">
+                    <span>Management</span>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-2">
+            <div class="chart" data-percent="92%" data-color="3498db">
+                  <span class="percent"></span>
+                  <div class="chart-text">
+                    <span>Adaptability</span>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-2">
+            <div class="chart" data-percent="100%" data-color="3498db">
+                  <span class="percent"></span>
+                  <div class="chart-text">
+                    <span>Teamwork</span>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-2">
+            <div class="chart" data-percent="85%" data-color="3498db">
+                  <span class="percent"></span>
+                  <div class="chart-text">
+                    <span>Communication</span>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-2">
+            <div class="chart" data-percent="100" data-color="3498db">
+                  <span class="percent"></span>
+                  <div class="chart-text">
+                    <span>Motivation</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,15 @@
     <Navigation />
     <AboutMe />
     <Resume />
+    <Skills />
   </div>
 </template>
 <script>
-import Home from '@/components/Home.vue'
-import Navigation from '@/components/Navigation.vue'
-import AboutMe from '@/components/AboutMe.vue'
-import Resume from '@/components/Resume.vue'
+import Home from '@/components/Home.vue';
+import Navigation from '@/components/Navigation.vue';
+import AboutMe from '@/components/AboutMe.vue';
+import Resume from '@/components/Resume.vue';
+import Skills from '@/components/Skills.vue';
 
 export default {
   components: {
@@ -18,6 +20,7 @@ export default {
     Navigation,
     AboutMe,
     Resume,
+    Skills,
   }
 }
 </script>
