3c246f28a8d588b39ec03e7ff6f48ed59fbfffe9
Javier Ortiz Saorín <javier.ortizsaorin@gmail.com>
2020-05-24 16:12:33 +0200

    feat: update the resume

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -4,11 +4,10 @@
       <h2 class="section-title wow fadeInUp">About Me</h2>
 
       <div class="row">
-
         <div class="col-md-4 col-md-push-8">
           <div class="biography">
             <div class="myphoto">
-              <img src="/images/myphoto.jpg" alt="">
+              <img src="/images/myphoto.jpg" alt />
             </div>
           </div>
         </div>
@@ -16,24 +15,50 @@
         <div class="col-md-8 col-md-pull-4">
           <div class="short-info wow fadeInUp">
             <h3>software development</h3>
-            <p>Senior Software Engineer with <b>10 years</b> of experience in <b>web applications, microservices and cloud solutions</b>. <br><br>
-            My primary role has been <b>technical lead</b> in every project I’ve worked during the last years, also being in direct contact with the clients to understand their business necessities and goals, and providing them the most effective high-tech solutions. <br><br>
-            With ample experience in <b>AngularJS, Vue.js, Node.js, Express.js, Javascript ES6/ES7, Docker, Kubernetes, AWS and commercetools.</b> <br><br>
-          </p>
+            <p>
+              Senior Software Engineer with
+              <b>more than 12 years</b> of experience in
+              <b>web applications, microservices and cloud solutions</b>.
+              <br />
+              <br />My primary role has been
+              <b>technical lead</b> in every project I’ve worked during the last years, also being in direct contact with the clients to understand their business necessities and goals, and providing them the most effective high-tech solutions.
+              <br />
+              <br />With ample experience in
+              <b>React, Vue.js, Node.js, Express.js, Gatsby.js, Docker, Kubernetes, AWS, Google Cloud and commercetools.</b>
+              <br />
+              <br />
+            </p>
           </div>
 
           <div class="short-info wow fadeInUp">
             <h3>leadership</h3>
-            <p>Furthermore, I established the software development processes, tools, and technologies as the <b>CTO</b> of the <b>startup</b> company I worked for the past three years. <br><br>
-              Being in that position, gave me the opportunity to use my skills and expertise to be part and leader of a growing team of more than 15 developers. </p>
+            <p>
+              Furthermore, I established the software development processes, tools, and technologies as the
+              <b>CTO</b> of the
+              <b>startup</b> company I worked for the past three years.
+              <br />
+              <br />Being in that position, gave me the opportunity to use my skills and expertise to be part and leader of a growing team of more than 15 developers.
+            </p>
 
             <ul class="list-check">
-              <li><font-awesome-icon class="icon" :icon="['fas', 'check']" />Microservices</li>
-              <li><font-awesome-icon class="icon" :icon="['fas', 'check']" />Cloud</li>
-              <li><font-awesome-icon class="icon" :icon="['fas', 'check']" />Technical Lead</li>
-              <li><font-awesome-icon class="icon" :icon="['fas', 'check']" />Full Stack Developer</li>
-              <li><font-awesome-icon class="icon" :icon="['fas', 'check']" />Kubernetes</li>
-              <li><font-awesome-icon class="icon" :icon="['fas', 'check']" />E-Commerce</li>
+              <li>
+                <font-awesome-icon class="icon" :icon="['fas', 'check']" />Microservices
+              </li>
+              <li>
+                <font-awesome-icon class="icon" :icon="['fas', 'check']" />Cloud
+              </li>
+              <li>
+                <font-awesome-icon class="icon" :icon="['fas', 'check']" />Technical Lead
+              </li>
+              <li>
+                <font-awesome-icon class="icon" :icon="['fas', 'check']" />Full Stack Developer
+              </li>
+              <li>
+                <font-awesome-icon class="icon" :icon="['fas', 'check']" />Kubernetes
+              </li>
+              <li>
+                <font-awesome-icon class="icon" :icon="['fas', 'check']" />E-Commerce
+              </li>
             </ul>
           </div>
         </div>
